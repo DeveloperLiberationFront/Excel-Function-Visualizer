ccadfe25f52c69ad505034884627d751629825b9
JustinAMiddleton <stngry09@msn.com>
2016-02-20 20:42:51 -0500

    More tests.

--- src/tests/SingleTests.java
+++ src/tests/SingleTests.java
@@ -1,7 +1,5 @@
 package tests;
 
-import static org.junit.Assert.*;
-
 import org.apache.poi.xssf.usermodel.XSSFEvaluationWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
@@ -34,7 +32,7 @@ public class SingleTests {
     TestUtils.compare(formula, result);
   }
   
-  //TODO: MissingArgPtg
+  //TODO: MissingArgPtg present, which included space in my parser's result.
   @Test
   public void test_03_blanksum() {
     String filename = "./testSheets/albert_meyers__1__1-25act.xlsx",
@@ -47,9 +45,29 @@ public class SingleTests {
     TestUtils.compare(formula, result);
   }
   
+  /**
+   * PROBLEM: Deals!#REF! becomes only #REF! when parsed.
+   * SOLUTION: In compare function, remove sheet name in formula string for those errors.
+   */
+  @Test
   public void test_04_referror() {
     String filename = "./testSheets/albert_meyers__1__1-25act.xlsx",
            formula = "IF(SUM(Deals!#REF!)=0,0,SUM(Deals!#REF!*Deals!#REF!)/SUM(Deals!#REF!))";
+    int    sheetNum = 0;
+
+    XSSFWorkbook wb = TestUtils.getWorkbook(filename);
+    XSSFEvaluationWorkbook parse = XSSFEvaluationWorkbook.create(wb);
+    String result = Parser.parseFormula(formula, sheetNum, parse);
+    TestUtils.compare(formula, result);
+  }
+  
+  /**
+   * PROBLEM: Third-party functions throw an error.
+   */
+  @Test
+  public void test_05_thirdparty() {
+    String filename = "./testSheets/albert_meyers__1__1-25act.xlsx",
+           formula = "IndGencost(D104,OFFSET(HeatRateStart,1,MATCH($A104,HeatRateNames,0)-1,250,1),$A105)";
     int    sheetNum = 0;
 
     XSSFWorkbook wb = TestUtils.getWorkbook(filename);
--- src/tests/TestUtils.java
+++ src/tests/TestUtils.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.openxml4j.opc.PackageAccess;
+import org.apache.poi.ss.formula.FormulaParseException;
 import org.apache.poi.ss.formula.FormulaParser;
 import org.apache.poi.ss.formula.FormulaParsingWorkbook;
 import org.apache.poi.ss.formula.FormulaRenderingWorkbook;
@@ -56,10 +57,10 @@ public class TestUtils {
         for (Cell cell : row) {
           
           if (cell.getCellType() == Cell.CELL_TYPE_FORMULA) {
-            String coord = i + ":" + cell.getRowIndex() + ":" + cell.getColumnIndex() + ":";
+            String coord = cellToString(cell);
             ++numOfTests;
             
-            String formula = cell.getCellFormula();
+            String formula = getCell(cell);
             Ptg[] tokens = FormulaParser.parse(formula, parse, FormulaType.CELL, i);
             
             String result = Parser.parseFormula(tokens, render);
@@ -73,24 +74,49 @@ public class TestUtils {
     System.out.println(numOfTests + " tests successful!");
   }
 
+  private static String getCell(Cell cell) {
+    String formula = "";
+    
+    try {
+      formula = cell.getCellFormula();
+    } catch (FormulaParseException e) {
+      System.err.println(cellToString(cell));
+      fail(e.getMessage());
+    }
+    
+    return formula;
+  }
+
   /**
    * 
    * @param formula
    * @param result
    */
   public static void compare(String formula, String result) {
-    String formulaNoWhite = removeWhiteSpace(formula),
-           resultNoWhite = removeWhiteSpace(result);
+    String formulaNoWhite = format(formula),
+           resultNoWhite = format(result);
     assertEquals(formulaNoWhite, resultNoWhite);
   }
   
   /* TODO: What about whitespace in quotes? */
   /**
-   * 
+   * 1. Remove white space
+   * 2. Remove sheets for errors.
    * @param str
    * @return
    */
-  public static String removeWhiteSpace(String str) {
-    return str.replaceAll("[ \t\r\n]", "");
+  public static String format(String str) {
+    return str
+            .replaceAll("[ \t\r\n]", "")
+            .replaceAll("\\w+!#", "#");   //Added in test_04
+  }
+  
+  /**
+   * 
+   * @param cell
+   * @return
+   */
+  private static String cellToString(Cell cell) {
+    return cell.getSheet().getSheetName() + ":" + cell.getRowIndex() + ":" + cell.getColumnIndex() + ":";
   }
 }
