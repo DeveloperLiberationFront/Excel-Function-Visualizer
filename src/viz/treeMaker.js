74df367ca274ee27134831721b162a19ea1e826f
JustinAMiddleton <stngry09@msn.com>
2016-03-10 23:25:38 -0500

    Color stuff.

--- src/viz/treeMaker.js
+++ src/viz/treeMaker.js
@@ -4,6 +4,7 @@
  * https://bl.ocks.org/mbostock/4339083
  * http://bl.ocks.org/robschmuecker/6afc2ecb05b191359862
  * http://bl.ocks.org/mbostock/3680999
+ * http://bl.ocks.org/robschmuecker/7880033
  */
 
 var margin = {
@@ -19,7 +20,16 @@ var margin = {
   square_side_half = square_side / 2,
 
   view_start_x = margin.left,
-  view_start_y = height / 2 + margin.top;
+  view_start_y = height / 2 + margin.top,
+
+  circle_col =    "#4C7B61",
+  circle_hover =  "#1D5134",
+  rect_col =      "#627884",
+  rect_hover =    "#35425B",
+  expand_col =    "#ADAD6B",
+  expand_hover =  "#898949"
+  empty_col =     "white",
+  empty_hover =   "lightgray";
 
 var scale; //To be determined when when tree chosen.
 
@@ -108,6 +118,8 @@ d3.json(src, function(error, json) {
       if (d.position != null && d._children.length > 10) {
         d._children.sort(function(a,b) { return b.frequency - a.frequency; });
         d._holding = d._children.splice(10);
+        d._children.splice(d._children.length, 0,
+          {"function":"", "frequency":-1, "parent":this});
       }
     } else {
       d.children = null;
@@ -159,28 +171,50 @@ function enterNode(node, src) {
   var nodeEnter = node.enter()
     .append("g")
     .attr("class", "node")
+    .attr("id", function(d) { return "n" + d.id; })
     .attr("transform", function(d) {
       var functionNode = d.depth % 2 == 0,
         y = src.y0 + (functionNode ? 0 : square_side_half),
         x = src.x0 + (functionNode ? 0 : square_side_half);
       return "translate(" + y + "," + x + ")";
-    })
-    .on("click", click);
+    });
 
-  var circles = nodeEnter.filter(function(d) { return d.depth % 2 == 0; }),
-    rects = nodeEnter.filter(function(d) { return d.depth % 2 != 0; })
+  var circles = nodeEnter.filter(function(d) {
+        return d.depth % 2 == 0 && d.frequency > 0;
+      }),
+      expand = nodeEnter.filter(function(d) {
+        return d.depth % 2 == 0 && d.frequency == -1;
+      }),
+      rects = nodeEnter.filter(function(d) { return d.depth % 2 != 0; })
 
-  enterCircle(circles, src);
-  enterRect(rects, src);
+  circles.on("click", click);
+  rects.on("click", click)
+    .on("mouseover", rect_mouseover)
+    .on("mouseleave", rect_mouseout);
+  expand.on("click", expandclick);
+
+  enterCircle(circles);
+  enterRect(rects);
+
+  expand.append("polygon")
+    .attr("height", "20")
+    .attr("width", "20")
+    .attr("points", "0,0 0,0 0,0")
+    //.attr("points", "-10,-5 0,15 10,-5")
+    .style("fill", expand_col)
+    .on("mouseover", function(d) { d3.select("#n" + d.id).select("polygon")
+      .style("fill", expand_hover); })
+    .on("mouseleave", function(d) { d3.select("#n" + d.id).select("polygon")
+      .style("fill", expand_col); })
 }
 
 /**
  * Creates the new function nodes.
  */
-function enterCircle(circles, src) {
+function enterCircle(circles) {
   circles.append("circle")
     .attr("r", 1e-6)
-    .style("fill", function(d) { return d._children ? "#4C7B61" : "white"; })
+    .style("fill", function(d) { return d._children ? circle_col : empty_col; })
     .on("mouseover", mouseover)
     .on("mousemove", mousemove)
     .on("mouseleave", mouseleave);
@@ -237,6 +271,15 @@ function mouseover(d) {
     .style("left", (d3.event.pageX + tip_x) + "px")
     .style("top", (d3.event.pageY + tip_y) + "px")
     .style("display", "inline");
+
+  var hovered = d3.select("#n" + d.id)
+    .select("circle");
+
+  hovered.style("fill", function(d) {
+    if (d._children) return circle_hover;     //Get darker color.
+    else if (d.children) return empty_hover;  //Get gray.
+    else return empty_col;                    //Don't change if no children.
+  });
 }
 
 /**
@@ -252,20 +295,28 @@ function mousemove(d) {
  */
 function mouseleave(d) {
   tip.style("display", "none");
+
+  var hovered = d3.select("#n" + d.id)
+    .select("circle");
+
+  hovered.style("fill", function(d) {
+    if (d._children) return circle_col;
+    else return empty_col;
+  });
 }
 
 /**
  * Create the rectangles which represent the argument
  * positions in a given function.
  */
-function enterRect(rects, src) {
+function enterRect(rects) {
   //Starts rectangle infinitesimally small, so updateNode brings it to size.
   rects.append("rect")
     .attr("width", 1e-6)
     .attr("height", 1e-6)
     .attr("x", -square_side_half)
     .attr("y", -square_side_half)
-    .style("fill", function(d) { return d._children ? "#627884" : "white"; });
+    .style("fill", function(d) { return d._children ? rect_col : empty_col; });
 
   rects.append("text")
     .text(function(d) { return d.position + 1; })
@@ -273,10 +324,24 @@ function enterRect(rects, src) {
     .attr("dy", 5);
 }
 
+function rect_mouseover(d) {
+  d3.select("#n" + d.id).select("rect").style("fill", function(d) {
+    if (d._children) return rect_hover;
+    else return empty_hover;
+  });
+}
+
+function rect_mouseout(d) {
+  d3.select("#n" + d.id).select("rect").style("fill", function(d) {
+    if (d._children) return rect_col;
+    else return empty_col;
+  });
+}
+
 /**
  * Transitions the new nodes to their intended position and shape.
  */
-function updateNode(node, src) {
+function updateNode(node) {
   var nodeUpdate = node.transition()
     .duration(duration)
     .attr("transform", function(d) {
@@ -286,16 +351,34 @@ function updateNode(node, src) {
   nodeUpdate.select("circle")
     .attr("r", function(d) { return scale(d.frequency); })
     .style("fill", function(d) {
-      return d._children ? "rgb(32, 114, 68)" : "white";
+      return d._children ? circle_col : empty_col;
     });
 
   nodeUpdate.select("rect")
     .attr("width", square_side)
     .attr("height", square_side)
-    .style("fill", function(d) { return d._children ? "#627884" : "white"; });
+    .style("fill", function(d) { return d._children ? rect_col : empty_col; });
+
+  nodeUpdate.select("polygon")
+  .attr("height", "20")
+  .attr("width", "20")
+  .attr("points", function(d) {
+    if (d.parent._holding)
+      return "-10,-5 0,15 10,-5";
+    else
+      return "-10,15 0,-5 10,15";
+  })
+  .style("fill", expand_col);
 
   nodeUpdate.select("text")
     .style("fill-opacity", 1);
+
+  nodeUpdate.attr("stroke-width", function(d) {
+    if (!d._children && d.frequency > 0) {
+      return "1px";
+    } else
+      return "0px";
+  })
 }
 
 /**
@@ -317,6 +400,11 @@ function exitNode(node, src) {
   nodeExit.select("rect")
     .attr("width", 1e-6)
     .attr("height", 1e-6);
+
+  nodeExit.select("polygon")
+    .attr("width", 1e-6)
+    .attr("height", 1e-6)
+    .attr("points", "0,0 0,0 0,0");
 
   nodeExit.select("text")
     .attr("fill-opacity", 1e-6);
@@ -371,4 +459,19 @@ function click(d) {
   }
 
   update(d);
+}
+
+function expandclick(d) {
+  var par = d.parent;
+
+  if (par._holding) {
+    par.children = par.children.concat(par._holding);
+    par._holding = null;
+  } else {
+    par._holding = par.children.splice(10)
+      .filter(function(i) { return i.frequency != -1; });
+    par.children.splice(par.children.length, 0, d);
+  }
+
+  update(par);
 }
