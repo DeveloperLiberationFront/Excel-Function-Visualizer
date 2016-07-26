4591aa112f5bdc328b8a83aaf4a0545cb179fea3
JustinAMiddleton <stngry09@msn.com>
2016-07-26 19:20:36 -0400

    End-to-end pipeline work.

---
+++ src/scripts/Orchard.java
@@ -0,0 +1,28 @@
+package scripts;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import core.FormulaToken;
+import core.FunctionNode;
+
+public class Orchard {
+  private final Map<String, FunctionNode> trees = new HashMap<String, FunctionNode>();
+  
+  /**
+   * 
+   * @param formula
+   */
+  public void add(FormulaToken formula) {
+    //TODO: Fill in
+  }
+  
+  /**
+   * 
+   */
+  public void flush(String outputDirectory) {
+    //Sorts
+    //Correct children
+    //Print to file
+  }
+}
---
+++ src/scripts/SheetAnalysis.java
@@ -0,0 +1,97 @@
+package scripts;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.openxml4j.opc.PackageAccess;
+import org.apache.poi.ss.formula.FormulaParsingWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFEvaluationWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import core.FormulaToken;
+import core.Parser;
+
+public class SheetAnalysis {
+  private static final String INPUT_DIRECTORY = "./sheets/ENRON/";
+  private static final String OUTPUT_DIRECTORY = "./src/viz/json";
+  
+  /**
+   * Ingests spreadsheets and outputs JSON files that describe the frequencies
+   * with which functions are used and how functions are combined. 
+   * TODO: Include example of what a JSON file looks like in this main comment.
+   * 
+   * @param args  No external args currently expected when running this file.
+   */
+  public static void main(String[] args) {  
+    SheetAnalysis analysis = new SheetAnalysis();
+    File sheetDirectory = new File(INPUT_DIRECTORY);    
+    
+    analysis.analyzeFilesIn(sheetDirectory);
+  }
+
+  /**
+   * Opens a directory and analyzes every spreadsheet within.
+   * Analysis here means to check every cell for formula. If the cell
+   * contains a formula, then store information about the formula's
+   * structure.
+   * 
+   * @param sheetDirectory  The directory which contains the spreadsheets
+   *                            you want to analyze.
+   */
+  public void analyzeFilesIn(File sheetDirectory) {
+    if (!sheetDirectory.isDirectory()) {
+      System.err.println("SSDirectory is not a directory!");
+      return;
+    }
+    
+    Orchard trees = new Orchard();
+    Queue<File> directoriesToAnalyze = new ArrayDeque<File>();    
+    directoriesToAnalyze.add(sheetDirectory);
+    
+    while (directoriesToAnalyze.isEmpty()) {
+      for (File file : sheetDirectory.listFiles()) {
+        if (file.getName().endsWith(".ignore")) {
+          continue;
+        } else if (file.isDirectory()) {
+          directoriesToAnalyze.add(file);
+          continue;
+        }
+        
+        XSSFWorkbook workbook = null;
+
+        try {
+          workbook = new XSSFWorkbook(OPCPackage.open(file, PackageAccess.READ));
+        } catch (IOException | InvalidFormatException ex) {
+          // TODO Auto-generated catch block
+          continue;
+        }
+        
+        FormulaParsingWorkbook parse = XSSFEvaluationWorkbook.create(workbook);
+        
+        //TODO: R1C1 Correction
+        //TODO: Error handling for parseFormula
+        
+        for (int i = 0; i < workbook.getNumberOfSheets(); ++i) {
+          Sheet sheet = workbook.getSheetAt(i);
+          for (Row row : sheet) {
+            for (Cell cell : row) {
+              if (cell.getCellType() == Cell.CELL_TYPE_FORMULA) {
+                FormulaToken formula = Parser.parseFormula(cell.getCellFormula(), parse, i);;
+                trees.add(formula);
+              }
+            }
+          }
+        }
+      }
+    }
+    
+    trees.flush(OUTPUT_DIRECTORY);
+  }
+}
